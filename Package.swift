15398ba395756cbb75f306193e70201a68373f42
trinhxuanminh2000 <trinhxuanminh2000@gmail.com>
2024-10-29 11:08:39 +0700

    gg ads 11.4.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
-    .package(name: "GoogleMobileAds", url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", revision: "11.11.0"),
+    .package(name: "GoogleMobileAds", url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", revision: "11.4.0"),
     .package(url: "https://github.com/firebase/firebase-ios-sdk", revision: "11.4.0"),
     .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework", revision: "6.13.2"),
     .package(url: "https://github.com/AppsFlyerSDK/PurchaseConnector-Dynamic", revision: "6.13.1"),
